c74a79d0d3734b554ad333fcbea4432e766a2a52
BartWesthoff-HRO <bartwesthoff2002@gmail.com>
2021-05-11 09:13:11 +0200

    fixed bugs building on phone. Fixed dummy code for adaptive graph

--- lib/Pages/SeePredictionLargePage.dart
+++ lib/Pages/SeePredictionLargePage.dart
@@ -20,7 +20,7 @@ class _SeePredictionLargePageState extends State<SeePredictionLargePage> {
     return Scaffold(
       backgroundColor: Colors.cyan[700],
       body: Container(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             Row(
@@ -104,7 +104,7 @@ class _SeePredictionLargePageState extends State<SeePredictionLargePage> {
     );
   }
 
-  LineChartData sampleData1({begin, goal, weeks}) {
+  LineChartData sampleData1({int begin, int goal, int weeks}) {
     return LineChartData(
       lineTouchData: LineTouchData(
         enabled: false,
@@ -140,11 +140,11 @@ class _SeePredictionLargePageState extends State<SeePredictionLargePage> {
       // minX altijd 0
       minX: 0,
       //maxX altijd duur training
-      maxX: weeks,
+      maxX: weeks.toDouble(),
       // max y = nulmeting begin + 20
-      maxY: begin + 20,
+      maxY: (begin + 20).toDouble(),
       //minY altijd doel -20
-      minY: goal - 20,
+      minY: (goal - 20).toDouble(),
       lineBarsData: linesBarData1(begin, goal, weeks),
     );
   }
@@ -177,7 +177,7 @@ class _SeePredictionLargePageState extends State<SeePredictionLargePage> {
 
 List<FlSpot> generateSpots(int begin, int goal, int weeks) {
   List<FlSpot> list = [];
-  for (var i = 0; i < weeks; i++) {
+  for (var i = 0; i < weeks + 1; i++) {
     var y = (goal - begin) / sqrt(weeks) * sqrt(i) + begin;
     list.add(FlSpot(i.toDouble(), y));
   }
