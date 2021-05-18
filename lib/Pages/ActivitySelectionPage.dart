efc20eb268c397ccd8bd63c0d0c1b32f188d6d7c
Reindert <ReindertKorver@gmail.com>
2021-05-18 16:05:45 +0200

    Activityselection now uses api

--- lib/Pages/ActivitySelectionPage.dart
+++ lib/Pages/ActivitySelectionPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:panda/Models/Activity.dart';
 import 'package:panda/Providers/ActivityProvider.dart';
+import 'package:panda/Providers/ApiProvider.dart';
+import 'package:panda/Providers/DBProvider.dart';
 import 'package:panda/widgets/CurrentActivities.dart';
 import 'package:panda/widgets/Logo.dart';
 
@@ -10,6 +14,41 @@ class ActivitySelectionPage extends StatefulWidget {
 
 //TODO: generiek maken met fitbitselection
 class _ActivitySelectionPageState extends State<ActivitySelectionPage> {
+  DateTime selectedDate = DateTime.now();
+  List<Activity> activities;
+  bool loading = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() {
+      loading = true;
+    });
+    var authState = await DBProvider.helper.getAuthState();
+    activities = await ApiProvider.getActivities(authState, selectedDate);
+    setState(() {
+      loading = false;
+    });
+  }
+
+  _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now().subtract(Duration(days: 1825)),
+        lastDate: DateTime.now().add(Duration(days: 1825)));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+      getData();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,8 +93,18 @@ class _ActivitySelectionPageState extends State<ActivitySelectionPage> {
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                ShowActivities(
-                    currentActvities: ActivityProvider.getActivity().toList()),
+                Text(DateFormat().format(selectedDate)),
+                OutlinedButton(
+                    child: Text("Datum selecteren"),
+                    onPressed: () {
+                      _selectDate(context);
+                    }),
+                if (loading)
+                  CircularProgressIndicator()
+                else if (activities != null)
+                  ShowActivities(currentActvities: activities)
+                else
+                  Text("Geen activiteiten om weer te geven.")
               ],
             ),
           ),
