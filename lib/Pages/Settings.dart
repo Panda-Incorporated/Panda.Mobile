46aa6db47b52255320bc35738053e3dc0f293b38
Sander Moerman <smoerman99@gmail.com>
2021-05-29 21:09:27 +0200

    Changed the layout for the settings page

--- lib/Pages/Settings.dart
+++ lib/Pages/Settings.dart
@@ -12,43 +12,46 @@ class _SettingsPageState extends State<SettingsPage> {
   String code;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Column(mainAxisSize: MainAxisSize.max, children: [
-          Container(
-            child: Column(
-              children: [
-                Text("Settings"),
-                OutlinedButton(
-                    child: Text("Authenticate"),
-                    onPressed: () async {
-                      var res = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AuthenticationPage()),
-                      );
-                      code = res.toString();
-                      setState(() {});
-                    }),
-                Text(code ?? "No code yet authenticate first"),
-                //todo: we can use this to talk to the fitbit api and get an accestoken (Reindert)
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Column(mainAxisSize: MainAxisSize.max, children: [
+            Container(
+              child: Column(
+                children: [
+                  Text("Settings"),
+                  OutlinedButton(
+                      child: Text("Authenticate"),
+                      onPressed: () async {
+                        var res = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AuthenticationPage()),
+                        );
+                        code = res.toString();
+                        setState(() {});
+                      }),
+                  Text(code ?? "No code yet authenticate first"),
+                  //todo: we can use this to talk to the fitbit api and get an accestoken (Reindert)
+                ],
+              ),
             ),
-          ),
-          OutlinedButton(
-              onPressed: () async {
-                for (var goal in await GoalProvider.getTempGoals()) {
-                  var id = await DBProvider.helper.insertGoal(goal);
-                  for (var act in GoalProvider.getTempActivitiesByGoal(id)) {
-                    await DBProvider.helper.insertActivity(act);
+            OutlinedButton(
+                onPressed: () async {
+                  for (var goal in await GoalProvider.getTempGoals()) {
+                    var id = await DBProvider.helper.insertGoal(goal);
+                    for (var act in GoalProvider.getTempActivitiesByGoal(id)) {
+                      await DBProvider.helper.insertActivity(act);
+                    }
                   }
-                }
-              },
-              child: Text("Fill database with 3 goals")),
-          Text(" warning dont touch more then once"),
-        ]),
-      ],
+                },
+                child: Text("Fill database with 3 goals")),
+            Text(" warning dont touch more then once"),
+          ]),
+        ],
+      ),
     );
   }
 }
