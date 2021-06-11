8fd52765009b720bfbba1009c1677e703d0f570e
Reindert <ReindertKorver@gmail.com>
2021-06-11 12:45:48 +0200

    settings listview

--- lib/Pages/Settings.dart
+++ lib/Pages/Settings.dart
@@ -40,70 +40,65 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Column(mainAxisSize: MainAxisSize.max, children: [
-            Container(
-              child: Column(
-                children: [
-                  Text(
-                    "Instellingen",
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                  NewGoalInputAndTextField(
-                      onEdittingComplete: onEditUsername,
-                      controller: controller,
-                      labelText: "Voer gebruikersnaam in",
-                      textString: "Gebruikersnaam wijzigen"),
-
-                  //todo: we can use this to talk to the fitbit api and get an accestoken (Reindert)
-                ],
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Column(children: [
+              Text(
+                "Instellingen",
+                style: TextStyle(fontSize: 24.0),
               ),
-            ),
-            Divider(),
-            OutlinedButton(
-                onPressed: () async {
-                  for (var goal in await GoalProvider.getTempGoals()) {
-                    var id = await DBProvider.helper.insertGoal(goal);
-                    for (var act in GoalProvider.getTempActivitiesByGoal(id)) {
-                      await DBProvider.helper.insertActivity(act);
+              NewGoalInputAndTextField(
+                  onEdittingComplete: onEditUsername,
+                  controller: controller,
+                  labelText: "Voer gebruikersnaam in",
+                  textString: "Gebruikersnaam wijzigen"),
+              Divider(),
+              OutlinedButton(
+                  onPressed: () async {
+                    for (var goal in GoalProvider.getTempGoals()) {
+                      var id = await DBProvider.helper.insertGoal(goal);
+                      for (var act
+                          in GoalProvider.getTempActivitiesByGoal(id)) {
+                        await DBProvider.helper.insertActivity(act);
+                      }
                     }
-                  }
-                },
-                child: Text("Fill database with 1 goal")),
-            OutlinedButton(
-                onPressed: () async {
-                  showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Let op!'),
-                      content: const Text(
-                          'Weet u zeker dat u alle data binnen deze app wilt verwijderen? Alle opgeslagen gegevens zoals doelen, activiteiten, gebruikersnaam etc. gaan verloren.'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text('Nee'),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            await DBProvider.helper.deleteDb();
-                            await Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (c) => InitialPage()));
-                          },
-                          child: const Text('Ja'),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                child: Text("Reset app")),
-          ]),
-        ],
+                  },
+                  child: Text("Fill database with 1 goal")),
+              OutlinedButton(
+                  onPressed: () async {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Let op!'),
+                        content: const Text(
+                            'Weet u zeker dat u alle data binnen deze app wilt verwijderen? Alle opgeslagen gegevens zoals doelen, activiteiten, gebruikersnaam etc. gaan verloren.'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Nee'),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              await DBProvider.helper.deleteDb();
+                              await Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (c) => InitialPage()));
+                            },
+                            child: const Text('Ja'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: Text("Reset app")),
+            ])
+          ],
+        ),
       ),
     );
   }
